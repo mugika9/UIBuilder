81878054acde2ded1919b540d80ab70b1471e581
ari <ari.pras1618@gmail.com>
2014-10-10 10:27:38 +0900

    first commit

---
+++ src/com/mugika/uibuilder/UIItem.java
@@ -0,0 +1,54 @@
+package com.mugika.uibuilder;
+
+public class UIItem {	
+	private String id;
+	private String src;
+	private float width;
+	private float height;
+	private float posX;
+	private float posY;
+	private String tag;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public float getWidth() {
+		return width;
+	}
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public float getPosX() {
+		return posX;
+	}
+	public void setPosX(float posX) {
+		this.posX = posX;
+	}
+	public float getPosY() {
+		return posY;
+	}
+	public void setPosY(float posY) {
+		this.posY = posY;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+}
---
+++ src/com/mugika/uibuilder/UIStage.java
@@ -0,0 +1,44 @@
+package com.mugika.uibuilder;
+
+import java.util.ArrayList;
+
+public class UIStage {				
+	private float width;
+	private float height;
+	private float translateY;
+	private float translateX;
+	private ArrayList items;
+	
+	
+	public float getWidth() {
+		return width;
+	}
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public float getTranslateY() {
+		return translateY;
+	}
+	public void setTranslateY(float translateY) {
+		this.translateY = translateY;
+	}
+	public float getTranslateX() {
+		return translateX;
+	}
+	public void setTranslateX(float translateX) {
+		this.translateX = translateX;
+	}
+	public ArrayList getItems() {
+		return items;
+	}
+	public void setItems(ArrayList items) {
+		this.items = items;
+	}
+	
+}
